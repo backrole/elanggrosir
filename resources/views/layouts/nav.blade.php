f862c2f0ebb923ae268395a2fd402f44e0ff8ce6
Achmat Bahrul Islam <bahrulfaqih99@gmail.com>
2019-11-18 13:05:42 +0700

    fixing navbar (cek responsive)

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,34 +1,5 @@
-<!-- <nav class="tw-bg-white tw-fixed tw-w-screen tw-shadow tw-mb-8 tw-py-3 tw-z-50">
-    <div class="tw-mx-4 tw-px-6 md:tw-px-0">
-        <div class="tw-flex tw-items-center tw-justify-center">
-            <div class="tw-mr-6">
-                <a href="{{ url('/') }}" class="tw-text-3xl tw-font-bold tw-text-blue-700 hover:tw-no-underline tw-font-sans">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-            </div>
-            <div class="tw-flex-1 tw-text-right">
-                @guest
-                    <a class="tw-no-underline hover:tw-no-underline tw-text-blue-700 tw-text-sm tw-p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    @if (Route::has('register'))
-                        <a class="tw-no-underline hover:tw-no-underline tw-text-blue-700 tw-text-sm tw-p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    @endif
-                @else
-                    <span class="tw-text-blue-700 tw-text-sm tw-pr-4">{{ Auth::user()->name }}</span>
-
-                    <a href="{{ route('logout') }}"
-                        class="tw-no-underline hover:tw-no-underline tw-text-blue-700 tw-text-sm tw-p-3"
-                        onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                        {{ csrf_field() }}
-                    </form>
-                @endguest
-            </div>
-        </div>
-    </div>
-</nav> -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="tw-mr-6">
+<nav class="row justify-content-between navbar navbar-expand-lg navbar-light bg-light">
+    <div class="tw-mr-2 col-10">
                     <a href="{{ url('/') }}" class="tw-text-3xl tw-font-bold tw-text-blue-700 hover:tw-no-underline tw-font-sans">
                         {{ config('app.name', 'Laravel') }}
                     </a>
@@ -36,8 +7,7 @@
         <button class="navbar-toggler tw-items-center" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon  tw-items-center"></span>
         </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="tw-flex-1 tw-text-left">
+            <div class="collapse navbar-collapse col-2" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
                             @guest
                                 <a class="nav-item nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -56,6 +26,5 @@
                                 </form>
                             @endguest
                             </div>
-                        </div>
             </div>
 </nav>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,31 @@
+<!-- <div class="tw-w-56 tw-bg-blue-800 tw-min-h-screen tw-py-4 tw-pt-8 tw-text-white">
+    <div class="tw-flex tw-flex-col">
+        <a href="{{ url('/admin/') }}" class="tw-py-4 tw-px-4 hover:tw-text-white hover:tw-bg-blue-600 hover:tw-no-underline tw-capitalize tw-text-gray-200">
+            <i class="fa fa-home tw-text-gray-500" aria-hidden="true"></i>
+            Dashboard
+        </a>
+        <a href="{{ url('/admin/supplier') }}" class="tw-py-4 tw-px-4 hover:tw-text-white hover:tw-bg-blue-600 hover:tw-no-underline tw-capitalize tw-text-gray-200">
+            <i class="fa fa-home tw-text-gray-500" aria-hidden="true"></i>
+            Supplier
+        </a>
+        <a href="{{ url('/admin/produk-masuk') }}" class="tw-py-4 tw-px-4 hover:tw-text-white hover:tw-bg-blue-600 hover:tw-no-underline tw-capitalize tw-text-gray-200">
+            <i class="fa fa-home tw-text-gray-500" aria-hidden="true"></i>
+            Produk Masuk
+        </a>
+        <a href="{{ url('/admin/laporan') }}" class="tw-py-4 tw-px-4 hover:tw-text-white hover:tw-bg-blue-600 hover:tw-no-underline tw-capitalize tw-text-gray-200">
+            <i class="fa fa-home tw-text-gray-500" aria-hidden="true"></i>
+            Laporan
+        </a>
+        <a href="{{ url('/admin/data') }}" class="tw-py-4 tw-px-4 hover:tw-text-white hover:tw-bg-blue-600 hover:tw-no-underline tw-capitalize tw-text-gray-200">
+            <i class="fa fa-home tw-text-gray-500" aria-hidden="true"></i>
+            Data
+        </a>
+        <a href="{{ url('/admin/setting') }}" class="tw-py-4 tw-px-4 hover:tw-text-white hover:tw-bg-blue-600 hover:tw-no-underline tw-capitalize tw-text-gray-200">
+            <i class="fa fa-home tw-text-gray-500" aria-hidden="true"></i>
+            Setting
+        </a>
+    </div>
+</div> -->
 <div class="tw-w-56 tw-bg-blue-800 tw-min-h-screen tw-py-4 tw-pt-8 tw-text-white">
     <div class="tw-flex tw-flex-col">
         <a href="{{ url('/admin/') }}" class="tw-py-4 tw-px-4 hover:tw-text-white hover:tw-bg-blue-600 hover:tw-no-underline tw-capitalize tw-text-gray-200">
